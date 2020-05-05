b8508c5508b44136fef9a52afc761a432975b778
Sam Vlas <62710119+MrSamFlash@users.noreply.github.com>
2020-05-05 15:02:48 +0200

    W1D2-4
    If/Else Statement

---
+++ Week1/Dag2/W1D2-4/uitsmijter.js
@@ -0,0 +1,15 @@
+const age = 35;
+const isFemale = true;
+const driverStatus = 'bob';
+
+if (age >= 18 && isFemale) {
+    console.log("Welkom! Treed binnen, jongedame!");
+} else {
+    console.log("Helaas! U wordt verzocht buiten te blijven.");
+}
+
+if (driverStatus == 'bob') {
+    console.log("Rij veilig!");
+} else {
+    console.log("Rij mee met uw bob!");
+}
