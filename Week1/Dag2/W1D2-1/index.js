d149fd85eda2876370dbf98a5d721fa46bc9c807
Sam Vlas <62710119+MrSamFlash@users.noreply.github.com>
2020-05-05 10:37:01 +0200

    W1D2-1
    Mijn eerste JavaScript speeltuin!

---
+++ Week1/Dag2/W1D2-1/index.js
@@ -0,0 +1,50 @@
+console.log('Hello Winc Academy');
+VM1089: 1 Hello Winc Academy
+undefined
+
+let name = 'Sam';
+undefined
+console.log(name);
+VM1119: 1 Sam
+undefined
+
+let if = 'Sam';
+VM1145: 1 Uncaught SyntaxError: Unexpected token 'if'
+
+console.log(3 + 2);
+VM1159: 1 5
+undefined
+
+console.log('Sam' + ' Vlas');
+VM1175: 1 Sam Vlas
+undefined
+
+console.log(66 + 2);
+VM1208: 1 68
+undefined
+
+console.log(99 % 8);
+VM1224: 1 3
+undefined
+
+console.log(63 * 7);
+VM1240: 1 441
+undefined
+
+console.log(99 - 87);
+VM1259: 1 12
+undefined
+
+console.log(86 / 4);
+VM1275: 1 21.5
+undefined
+
+let age = 26;
+undefined
+typeof age;
+"number"
+
+let age = 'zes-en-twintig';
+undefined
+typeof age;
+"string"
